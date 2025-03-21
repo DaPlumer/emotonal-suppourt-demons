68ec6f46c830080358bb85550d44764cfaa2a654
DaPlumer <daplumer271@gmail.com>
2025-03-21 16:56:44 -0400

    Changed class info

--- src/main/java/esd/entity/ModEntities.java
+++ src/main/java/esd/entity/ModEntities.java
@@ -9,23 +9,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 /**
- * The ModEntities class is responsible for registering and managing custom entity types
- * within the EmotionalSupportDemons mod.
- *
- * This class handles the initialization of entities, including their registration
- * with the game registry. It defines the properties and behaviors of the entity types,
- * as well as ensures that they are properly logged and identified.
- *
- * Entity registration is achieved through the use of the Fabric API's Registry utilities,
- * which enable seamless integration with the game. Among other entities, this class defines
- * the DemonEntity, a custom creature with unique traits and interactions.
- *
- * Methods:
- * - registerEntities(): Logs the registration process for custom entities in this mod.
- *
- * Fields:
- * - DEMON: Represents the registered instance of the custom DemonEntity type, created and registered
- *   with specific attributes such as spawn group and dimensions.
+ * This is currently just a wrapper for the Emotional Support demon
  */
 public class ModEntities {
     public static final EntityType<DemonEntity> DEMON = Registry.register(Registries.ENTITY_TYPE,
