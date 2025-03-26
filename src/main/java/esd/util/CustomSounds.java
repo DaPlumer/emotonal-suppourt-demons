44de8225fde4ff6956fd8cc3c7379a7690f8a8a6
DaPlumer <daplumer271@gmail.com>
2025-03-26 19:22:27 -0400

    Adding sounds

--- src/main/java/esd/util/CustomSounds.java
+++ src/main/java/esd/util/CustomSounds.java
@@ -8,21 +8,15 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 public class CustomSounds {
-    private CustomSounds() {
-        // private empty constructor to avoid accidental instantiation
-    }
-
-    // ITEM_METAL_WHISTLE is the name of the custom sound event
-    // and is called in the mod to use the custom sound
     public static final RegistryEntry.Reference<SoundEvent> CRASH = registerReference("crash");
     public static final SoundEvent START_FALL = registerSound("start_fall");
     public static final SoundEvent SQUEAK = registerSound("squeak");
 
-    // actual registration of all the custom SoundEvents
     private static SoundEvent registerSound(String id) {
         Identifier identifier = Identifier.of(EmotionalSuppourtDemons.MOD_ID, id);
         return Registry.register(Registries.SOUND_EVENT, identifier, SoundEvent.of(identifier));
     }
+
     @SuppressWarnings("SameParameterValue")
     private static RegistryEntry.Reference<SoundEvent> registerReference(String id) {
         return registerReference(Identifier.of("esd", id));
@@ -36,11 +30,7 @@ public class CustomSounds {
         return Registry.registerReference(Registries.SOUND_EVENT, id, SoundEvent.of(soundId));
     }
 
-    // This static method starts class initialization, which then initializes
-    // the static class variables (e.g. ITEM_METAL_WHISTLE).
     public static void initialize() {
         EmotionalSuppourtDemons.LOGGER.info("Registering " + EmotionalSuppourtDemons.MOD_ID + " Sounds");
-        // Technically this method can stay empty, but some developers like to notify
-        // the console, that certain parts of the mod have been successfully initialized
     }
 }
